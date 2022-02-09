51c1457646483dd1a48a094c4456100533036f14
Maxi Spoltore <maxi.spoltore@fizzmod.com>
2022-02-09 01:47:46 -0300

    init timer functionality

--- components/GameOne/Match.js
+++ components/GameOne/Match.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import useSWR from 'swr';
+import { fetcher } from '../../data/api';
 
-const Match = ({ text }) => {
-	return <div>{text}</div>;
+const Match = ({ data }) => {
+	const { data: pokemonData } = useSWR(data.url, fetcher);
+	const { sprites = {}, name } = pokemonData || {};
+	return (
+		<div className='flex flex-col items-center m-2 w-36'>
+			<img src={sprites['front_default']} alt={name} />
+			<p className='font-pokemon-solid capitalize tracking-[.15em]'>{name}</p>
+		</div>
+	);
 };
 
 export default Match;
--- components/GameOne/index.js
+++ components/GameOne/index.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import useSWR from 'swr';
 import { fetcher } from '../../data/api';
 import LetterCube from './LetterCube';
 import Match from './Match';
+import Countdown from 'react-countdown';
 
 const keyCodeMap = {
 	backspace: 8,
@@ -12,16 +13,17 @@ const keyCodeMap = {
 
 const GameOne = () => {
 	const [guess, setGuess] = useState('');
-	const [matches, setMatches] = useState([]);
+	const [matches, setMatches] = useState(new Set());
+	const [gameOver, setGameOver] = useState(false);
 	const { data, error } = useSWR('https://pokeapi.co/api/v2/pokemon?limit=151', fetcher);
+	const gameOverRef = useRef(null);
 
-	const submitGuess = () => {
+	const submitGuess = async () => {
 		if (!guess) return;
-		console.log({ submittedGuess: guess });
 		const { results: pokemonList } = data || [];
-		const validMatch = pokemonList.some(pokemon => pokemon.name === guess);
+		const validMatch = pokemonList.find(pokemon => pokemon.name === guess);
 		if (validMatch) {
-			setMatches(prev => [...prev, guess]);
+			setMatches(prev => new Set(prev.add(validMatch)));
 			setGuess('');
 		} 
 	}
@@ -43,7 +45,7 @@ const GameOne = () => {
 	const renderGuess = () => {
 		const guessArr = guess.split('');
 		return (
-			<div className='flex'>
+			<div className='flex mt-6'>
 				{guessArr.map((char, idx) => {
 					const key = `character-${char}-${idx}`;
 					return <LetterCube key={key} char={char} />
@@ -52,14 +54,27 @@ const GameOne = () => {
 		);
 	};
 
-	const renderMatches = () => (
-		<div className='flex w-100'>
-			{!!matches.length && matches.map((match, idx) => {
-				const key = `match-${match}-${idx}`;
-				return <Match key={key} text={match} />
-			})}
-		</div>
-	);
+	const renderMatches = () => {
+		const matchesArr = Array.from(matches);
+		return (
+			<div className='flex flex-wrap w-100 bg-red-200 rounded-lg max-h-[320px] overflow-y-auto'>
+				{!!matchesArr.length && matchesArr.map((match, idx) => {
+					const key = `match-${match}-${idx}`;
+					return <Match key={key} data={match} />
+				})}
+			</div>
+		);
+	};
+
+	const renderTimer = ({ minutes, seconds, completed }) => {
+		if (completed) {
+			return <h5 ref={gameOverRef}>Time's up!</h5>
+		} else {
+			const formatTime = time => +time < 10 ? `0${time}` : time;
+			const formattedSeconds = formatTime(seconds);
+			return <span>{`${minutes}:`}{formattedSeconds}</span>
+		}
+	}
 
 	useEffect(() => {
 		window.addEventListener('keydown', handleGuess);
@@ -77,12 +92,32 @@ const GameOne = () => {
 	useEffect(() => {
 		console.log({ matches });
 	}, [matches])
+
+	useEffect(() => {
+		if (gameOverRef && gameOverRef.current) {
+			setGameOver(true);
+		}
+	}, [gameOverRef])
 	
 	return (
-		<div className='flex flex-column w-100'>
-			{renderMatches()}
-			{renderGuess()}
-		</div>
+		<>
+			{gameOver 
+				? <h2>Time's up!</h2>
+				: (
+				<div>
+					<div className='flex flex-col items-center w-9/12 mx-auto'>
+						{renderMatches()}
+						{renderGuess()}
+					</div>
+					<div>
+						<Countdown
+							date={Date.now() + 10000}
+							renderer={renderTimer}
+						/>
+					</div>
+				</div>
+				)}
+		</>
 	);
 };
 
